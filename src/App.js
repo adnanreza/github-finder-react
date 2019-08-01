692682c9921279b58907283f62fb2eeb0314250a
Adnan Reza <adnan.reza@outlook.com>
2019-08-01 11:28:29 -0700

    Refactor to use PropTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar.js";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello AR</h1>
+        <Navbar />
       </div>
     );
   }
